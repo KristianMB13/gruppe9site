2970dc96259474da8cff8c992bb27ed39d9d9c7c
KristianMB13 <kristian.bergedalen@hotmail.com>
2026-01-06 13:01:27 +0100

    Forsøk 3: fikse lenken til status siden

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppRoutes } from './router'
 
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
